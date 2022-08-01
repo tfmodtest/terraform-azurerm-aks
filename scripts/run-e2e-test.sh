e1ba5e968278c24cf5e6209a5bde4052ad561d2c
zjhe <hezijie@microsoft.com>
2022-08-01 15:00:43 +0800

    Use centralized CI script repo as pipeline script.

--- scripts/run-e2e-test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "==> Running E2E Tests..."
-set -e
-cd ./test/e2e
-go mod tidy
-echo "==> go test"
-go test -v -p=1 -timeout=120m ./...
--- scripts/run-golangci-lint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "==> Checking test go code against linters..."
-cd ./test
-go mod tidy
-echo "run golangci-lint ..."
-golangci-lint run --timeout 1h ./...
